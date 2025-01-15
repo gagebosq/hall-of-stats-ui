4a0bf5d7b3a7b4f2a02cc717cca655fc5b1a89e3
swagatron300 <gage.bosquez@gmail.com>
2025-01-15 16:32:23 -0600

    added sidebar

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,40 @@
 import logo from './assets/logo.jpg';
+import {Sidebar, Menu, MenuItem, sidebarClasses} from 'react-pro-sidebar';
 import './App.css';
 
 function App() {
     return (
-        <>
-            <div>
-                <img src={logo} className="logo" alt="logo" />
-            </div>
             <div className="sidebar">
-                <h2>Sidebar</h2>
-                <ul>
-                    <li><a href="#" >Pitchers</a></li>
-                    <li><a href="#" >Hitters</a></li>
-                    <li><a href="#" >Teams</a></li>
-                </ul>
+                <div>
+                    <img src={logo} className="logo" alt="logo"/>
+                </div>
+                <div className="navigation">
+                    <Sidebar
+                        rootStyles={{
+                        [`.${sidebarClasses.container}`]: {
+                            backgroundColor: 'white',
+                            left: '0px'
+                        },
+                    }}
+                        >
+                        <Menu
+                            menuItemStyles={{
+                                button: {
+                                    // the active class will be added automatically by react router
+                                    // so we can use it to style the active menu item
+                                    [`&.active`]: {
+                                        backgroundColor: '#000000',
+                                        color: '#000000FF',
+                                    },
+                                },
+                            }}>
+                            <MenuItem> Pitchers </MenuItem>
+                            <MenuItem> Hitters </MenuItem>
+                            <MenuItem> Teams </MenuItem>
+                        </Menu>
+                    </Sidebar>
+                </div>
             </div>
-        </>
     );
 }
 
